4666da1633162fc051ea5e21957066d3d4b82a89
Daniel Hong <dan910803@gmail.com>
2025-03-06 15:49:14 +0800

    20250306

--- Hw week 02.R
+++ Hw week 02.R
@@ -1,7 +1,9 @@
 # 1a. Generate 10000 random numbers from Gaussian distribution with mean=20 and variance =10, and plot the distribution.
-
-g_dis <- rnorm(10000, 20, 10)
-
+#variance=10, sd=square root 10!!!
+g_dis1 <- rnorm(10000)
+g_dis <- g_dis1  * 10 + 20
+mean(g_dis)
+sd(g_dis)
 hist(g_dis)
 
 # 1b. Generate 10000 random numbers from Binomial distribution with p=0.5 and n=40, and plot the distribution.
@@ -46,3 +48,63 @@ candi <- function (m, w){
 }
 
 candi(34, 16)
+
+
+
+x <- rnorm(10000) + 20
+mean(x)
+y <- x * 10
+sd(y)
+
+
+#generate random sequence every time and choose the first two
+#remove the first two
+
+candi2 <- function (m, w){
+  selected <- numeric(0)
+  
+  for(week in 1:w){
+    repeat{
+      num1 <- round(runif(1, 1, m))
+      num2 <- round(runif(1, 1, m))
+      
+      if(num1 != num2 && !(num1 %in% selected) && !(num2 %in% selected)){
+        print(c(num1, num2))
+        selected <- c(selected, num1, num2)
+        break
+      }
+    }
+  }
+}
+candi2(39, 15)
+
+candi3 <- function(m){
+  if (!exists("used", envir = .GlobalEnv)) {
+    assign("used", numeric(0), envir = .GlobalEnv)
+  }  
+  available <- setdiff(1:m, used)
+  
+  x <- runif(length(available))
+  candidate <- available[order(x)][1:2]
+  
+  assign("used", c(used, candidate), envir = .GlobalEnv)
+  
+  return(candidate)
+}
+set.seed(123)
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+print(candi3(30))
+
